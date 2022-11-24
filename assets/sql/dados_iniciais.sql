05ebd45693da8ede35d88ebaa1f5d5ba3b656c04
MariaVii <mav.andraade@gmail.com>
2022-11-24 14:14:21 +0000

    Preparacao Da Base De Dados

---
+++ assets/sql/dados_iniciais.sql
@@ -0,0 +1,11 @@
+INSERT INTO cardapio(id, nome) VALUES
+       (1, 'x-salada'),
+       (2, 'x-churrasco'),
+       (3, 'x-tudo'),
+       (4, 'x-bacon');
+
+      INSERT INTO lanche (id, ) VALUES
+       (1, 'pequeno', 2)
+       (2, 'médio', 1),
+       (3, 'grande', 3),
+       (4, 'extra', 4); 
